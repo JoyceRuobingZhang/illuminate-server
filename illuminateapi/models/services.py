53a37879d34254b4e7347944ef7d39549c11ff86
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-03-05 12:39:45 -0600

    updated service model

--- illuminateapi/models/services.py
+++ illuminateapi/models/services.py
@@ -6,19 +6,11 @@ class Services(models.Model):
     # img
     name = models.CharField(max_length=55)
     location = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.DecimalField(max_digits=15, decimal_places=6)
+    longitude = models.DecimalField(max_digits=15, decimal_places=6)
     type = models.CharField(max_length=55)
     email = models.CharField(max_length=55)
     phone = models.CharField(max_length=25)
-    rating = models.FloatField()
+    rating = models.DecimalField(max_digits=15, decimal_places=6)
     
-    # for custom properties that are not stored in the database
-    @property
-    def joined(self):
-        return self.__joined
-    
-    @joined.setter
-    def joined(self, value):
-        self.__joined = value
         
