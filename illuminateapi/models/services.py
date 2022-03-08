10cb048e3025fa080aa0a02cba184c9312181f51
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-03-07 22:59:09 -0600

    added filtering

--- illuminateapi/models/services.py
+++ illuminateapi/models/services.py
@@ -10,5 +10,6 @@ class Services(models.Model):
     email = models.CharField(max_length=55)
     phone = models.CharField(max_length=25)
     rating = models.DecimalField(max_digits=15, decimal_places=6)
-    
+    sliding_scale = models.BooleanField(default=True)
+    online = models.BooleanField(default=True)
         
