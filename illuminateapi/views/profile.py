cebe0f8c5442f397b8f35083700facb59b291a07
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-03-12 14:35:37 -0600

    added user serializer for profile

--- illuminateapi/views/profile.py
+++ illuminateapi/views/profile.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 from illuminateapi.models import AppUser
 from illuminateapi.views.event import EventSerializer
 
@@ -15,10 +15,16 @@ def get_auth_profile(request):
     return Response(serializer.data)
 
 
+class UserSerializer(serializers.ModelSerializer):
+    """JSON serializer for appuser's related Django user"""
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username')
+
 class AppUserSerializer(serializers.ModelSerializer):
 
     signed_up_events = EventSerializer(many=True)
-
+    user = UserSerializer(many=False)
     class Meta:
         model = AppUser
-        fields = ('id', 'bio', 'profile_img', 'signed_up_events')
+        fields = ('id', 'bio', 'profile_img', 'signed_up_events', 'user')
