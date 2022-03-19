0894650c3f8a980ef9e0f678f7020c18bd236324
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-03-19 11:26:37 -0500

    added like

--- illuminateapi/models/appuser.py
+++ illuminateapi/models/appuser.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,4 +9,4 @@ class AppUser(models.Model):
     bio = models.CharField(max_length=50)
     profile_img = models.FileField(upload_to='photos/%Y/%m/%d/', null=True)
     signed_up_events = models.ManyToManyField('illuminateapi.Event', related_name='signed_up_by') # attendees
-    
+    liked_posts = models.ManyToManyField('illuminateapi.Post', related_name='liked_by')
